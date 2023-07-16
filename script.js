ffd708d69f8cad4f06022fcfa79d157430438865
vv-yugoff <vv.yugoff@gmail.com>
2023-07-16 17:25:56 +0500

    Запоминнание рецензий по ответам + страница с результатами

--- script.js
+++ script.js
@@ -8,7 +8,11 @@ const questions = [
             Полезные обучения - те, что стимилируют ваш профессиональный и личностный рост.
             (Обучения техникам продаж, деловой этикет, язык тела в профессии и т.д.)`,
         answers: ['2-3 раза в месяц', '1 раз в месяц', 'реже одного раза в месяц', 'все знания добываю самостоятельно'],
-        points: [3, 2, 1, 0],
+        // points: [3, 2, 1, 0],
+        reviews: ['В твоем агенстве следят за уровнем знаний своих агентов и это похвально. Мы надеемся, что ообучения, которые ты проходишь помогают становиться настоящим профессионалом, а также развивают тебя как личность.',
+        'В твоем агестве проводятся ежемесячные ообучения и это отлично, но согласись, нет предела совершенству? Мы надеемся, что обучения влияют не только на твою профессиональную деятельность, но иразвивают тебя как личность.',
+        'Увы, твое агенство как-будто не очень заинтересовано в твоем профессиональном и личностном росте. Обучающие мероприятия проводятся исключительно редко, судя по твоему ответу.'
+        ],
     },
     {
         number: 2,
@@ -33,13 +37,6 @@ const questions = [
     },
     {
         number: 5,
-        category: 'Обязательства',
-        question: 'Есть ли у вас обязательства по посещению офиса?',
-        answers: ['нужно быть в офисе каждый день и отчитываться о проделанной работе', 'нужно быть в офисе раз в неделю', 'необязательно посещать офис'],
-        points: [2, 1, 0],
-    },
-    {
-        number: 6,
         category: 'Узнать честность агенства',
         question: 'Давайте посчитаем, сколько суммарно вы отдаете за месяц своему агенству недвижимости',
         answers: ['Среднее количество сделок в месяц:', 'Сколько из этих сделок вы обычно получаете от АН, в котором вы работает?', 'Средний чек по сделке:', 'Какой процент со сделки вы отдаете в свое агенство недвижимости?'],
@@ -62,9 +59,8 @@ const submitBtn = document.querySelector('#submit');
 
 // Переменные опросника
 let score = 0; // Количество очков
-let questionIndex = 0; // Номер вопроса
+let questionIndex = 1; // Номер вопроса
 
-// clearPage();
 showQuestion();
 submitBtn.addEventListener('click', checkAnswer);
 
@@ -79,22 +75,22 @@ function showQuestion() {
     let questionTemplate = '';
 
     const categoryTemplate = `<h3 class="header-question--category" id="category">%category%</h3>`;
-    const category = categoryTemplate.replace('%category%', questions[questionIndex]['category']);
+    const category = categoryTemplate.replace('%category%', questions[questionIndex - 1]['category']);
     categoryContainer.innerHTML = category;
 
     const numberTemplate = `<p class="header-question--numbers">%number% из 5</p>`;
-    const number = numberTemplate.replace('%number%', questions[questionIndex]['number']);
+    const number = numberTemplate.replace('%number%', questions[questionIndex - 1]['number']);
     numberContainer.innerHTML = number;
 
     const headerTemplate = `<h3 class="header-question--category">%title%</h3>`;
-    const title = headerTemplate.replace('%title%', questions[questionIndex]['question']);
+    const title = headerTemplate.replace('%title%', questions[questionIndex - 1]['question']);
     headerContainer.innerHTML = title;
 
-    for ([index, answerText] of questions[questionIndex]['answers'].entries()) {
+    for ([index, answerText] of questions[questionIndex - 1]['answers'].entries()) {
         console.log(index + 1, answerText);
         index++;
 
-        if (questionIndex !== questions.length - 1) {
+        if (questionIndex - 1 !== questions.length - 1) {
             questionTemplate = 
                 `<li>
                     <label>
@@ -111,8 +107,6 @@ function showQuestion() {
                 </li>`;
         }
 
-        console.log(questionTemplate);
-
         const answerHTML = questionTemplate
             .replace('%answer%', answerText)
             .replace('%index%', index);
@@ -122,33 +116,76 @@ function showQuestion() {
 }
 
 function checkAnswer() {
+    // Массив для значений из текстовых полей
+    const textInputArray = [];
+
     // Находим выбранную радиокнопку
     const checkedRadio = listContainer.querySelector('input[type="radio"]:checked');
+
+    // Находим все текстовые поля и их значения
+    // const textInputs = listContainer.querySelectorAll('input[type="text"]');
+
+    // textInputs.forEach(item => {
+    //     console.log(item.value);
+    //     textInputArray.push(item.value);
+    // });
+    // console.log(textInputs);
+    // console.log(textInputArray);
     
-    // Если ответ не выбран, то выходим из функции
+    // Если ответ не вы  бран, то выходим из функции
     if (!checkedRadio) {
         submitBtn.blur();
-        return
-    }
+        alert('Выберите ответ');
+        // return
+    } else if (listContainer.querySelectorAll('input[type="text"]').length !== 0) {
+        console.log('ТОГДА НАДО ПРОВЕРЯТЬ ТЕКСТОВЫЕ ПОЛЯ');
+        listContainer.querySelectorAll('input[type="text"]').forEach(item => {
+            console.log(item.value);
+            textInputArray.push(item.value);
+        });
+        console.log(listContainer.querySelectorAll('input[type="text"]'));
+        console.log(textInputArray);
+    } else {
+        // Узнаем номер ответа пользователя
+        const userAnswer = parseInt(checkedRadio.value);
+        console.log('Номер ответа пользователя - ', userAnswer);
+        
+        if (questions[questionIndex - 1]['reviews']) {
+            // Запоминаем рецензии по ответу
+            const reviewsList = [];
+            for ([index, review] of questions[questionIndex - 1]['reviews'].entries()) {
+                console.log(userAnswer);
+                console.log(index);
+                console.log(review);
 
-    // Узнаем номер ответа пользователя
-    const userAnswer = parseInt(checkedRadio.value);
-    console.log('Номер ответа пользователя - ', userAnswer);
-
-    // После ответа - увеличиваем счет
-    for ([scoreIndex, points] of questions[questionIndex]['points'].entries()) {
-        console.log(scoreIndex, points);
-        scoreIndex++;
-
-        if (userAnswer === scoreIndex) {
-            score += points;
+                if (userAnswer === index + 1) {
+                    console.log(document.querySelector('#answers-list'));
+                    const list = document.querySelector('#answers-list');
+                    reviewsList.push(review);
+                }
+            }
+            console.log(reviewsList);
+        } else {
+            console.log('Нет reviews');
         }
     }
-    console.log('Итог:', score);
 
-    if (questionIndex !== questions.length - 1) {
-        console.log('a', questionIndex);
-        console.log('b', questions.length - 1);
+
+
+    // После ответа - увеличиваем счет
+    // for ([scoreIndex, points] of questions[questionIndex]['points'].entries()) {
+    //     console.log(scoreIndex, points);
+    //     scoreIndex++;
+
+    //     if (userAnswer === scoreIndex) {
+    //         score += points;
+    //     }
+    // }
+    // console.log('Итог:', score);
+
+    if (questionIndex !== questions.length) {
+        console.log('НОМЕР ВОПРОСА: ', questionIndex);
+        console.log('ВСЕГО ВОПРОСОВ: ', questions.length);
         
         console.log('Это не последний вопрос');
         questionIndex++;
@@ -158,6 +195,7 @@ function checkAnswer() {
         showQuestion();
     } else {
         console.log('Последний вопрос');
+        clearPage();
         // Страница с результатами
         showResults();
     }
@@ -165,4 +203,13 @@ function checkAnswer() {
 
 function showResults() {
     console.log('РЕЗУЛЬТАТЫ');
+    console.log(document.querySelector('#category'));
+    const header = document.querySelector('header');
+    header.classList.add('hidden');
+    // document.querySelector('body').removeChild(header);
+    // document.querySelector('#category').textContent = 'Рецензия';
+    document.querySelector('#question-title').textContent = `
+        Спасибо, что ты нашел время, чтобы пройти этот тест, а теперь давай перейдем к анализу агенства, в котором ты работаешь.
+    `
+    // document.querySelector('section').textContent = 'Добрый день';
 }
